ce427da0d7151e895dff088fd6f56ccd3b60de61
Shaked Flur <sflur@google.com>
2026-02-10 11:21:28 +0100

    Compose affine map and operands when simplifying
    This is needed when previous calls optimized other ops.

--- mlir/lib/Dialect/Affine/Analysis/Utils.cpp
+++ mlir/lib/Dialect/Affine/Analysis/Utils.cpp
@@ -2071,7 +2071,9 @@ FailureOr<AffineValueMap> mlir::affine::simplifyConstrainedMinMaxOp(
   Builder builder(ctx);
   AffineMap map =
       isMin ? cast<AffineMinOp>(op).getMap() : cast<AffineMaxOp>(op).getMap();
-  ValueRange operands = op->getOperands();
+  SmallVector<Value> operands =
+      isMin ? cast<AffineMinOp>(op).getOperands() : cast<AffineMaxOp>(op).getOperands();
+  affine::fullyComposeAffineMapAndOperands(&map, &operands);
   unsigned numResults = map.getNumResults();
 
   // Add a few extra dimensions.
